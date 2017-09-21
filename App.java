54ee2b53971f47cf72629220cdd3dc93838da168
Mohamed Nagy <mohamed.mahmoud@datagearbi.com>
2017-09-21 15:24:34 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,57 +1,18 @@
 import java.util.Scanner;
-
+link : http://codeforces.com/contest/112/problem/A
 public class App {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		String word1, word2 = "";
-		int weight1 = 0;
-		int weight2 = 0;
-		word1 = scanner.nextLine();
-		word2 = scanner.nextLine();
-		for (int i = 0; i < word1.length(); i++) {
-			char ch = word1.charAt(i);
-			if (Character.isLowerCase(ch)) {
-				weight1 += getLowerCharOrder(ch);
-			} else {
-				weight1 += getUpperCharOrder(ch);
-			}
-		}
-
-		for (int i = 0; i < word2.length(); i++) {
-			char ch = word2.charAt(i);
-			if (Character.isLowerCase(ch)) {
-				weight2 += getLowerCharOrder(ch);
-			} else {
-				weight2 += getUpperCharOrder(ch);
-			}
-		}
-
-		if (weight1 > weight2) {
-			System.out.println("1");
-		} else if (weight1 < weight2) {
-			System.out.println("-1");
-		} else {
-			System.out.println("0");
-		}
-	}
-
-	public static int getLowerCharOrder(char ch) {
-		String alph = "abcdefghijklmnopqrstuvwxyz";
-		int index = 0;
-		for (int i = 0; i < alph.length(); i++) {
-			index = alph.indexOf(ch);
-		}
-		return index;
-	}
-
-	public static int getUpperCharOrder(char ch) {
-		String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int index = 0;
-		for (int i = 0; i < alph.length(); i++) {
-			index = alph.indexOf(ch);
-		}
-		return index;
-	}
+		
+		Scanner in = new Scanner(System.in);
+		String firstString = in.next();
+		String secondString = in.next();
+		firstString = firstString.toLowerCase();
+		secondString = secondString.toLowerCase();
+		int result = firstString.compareTo(secondString);
+		if(result == 0) System.out.println("0");
+		else if(result > 0) System.out.println("1");
+		else System.out.println("-1");
+		in.close();
 }
